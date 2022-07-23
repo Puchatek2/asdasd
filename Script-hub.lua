23d5d1c7c164c32f05c33b5a8a8ae1f17543cd37
Puchatek2 <100354227+Puchatek2@users.noreply.github.com>
2022-07-23 13:18:28 +0000

    Update Script-hub.lua

--- Script-hub.lua
+++ Script-hub.lua
@@ -1,3 +1,11 @@
 if game.PlaceId == 9551640993 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Puchatek2/asdasdasdasdasdasd/main/main.lua"))()
+  local HWIDTable = loadstring(game:HttpGet("https://pastebin.com/raw/UkixRfi9"))()
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+for i,v in pairs(HWIDTable) do
+    if v == HWID then
+        print ("Hardware ID Passed, You can load the script now...")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Puchatek2/asdasd/main/Script-hub.lua"))()
+    end
 end
