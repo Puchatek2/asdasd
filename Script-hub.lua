4c05973bea5b0ed96d5edb43c37ad3e677dc792d
Puchatek2 <100354227+Puchatek2@users.noreply.github.com>
2022-07-23 16:29:48 +0000

    Update Script-hub.lua

--- Script-hub.lua
+++ Script-hub.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 9551640993 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Puchatek2/asdasdasdasdasdasd/main/main.lua"))()
+  elseif game.PlaceId == 9551640993 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Puchatek2/rebirthchampions/main/script.lua"))()
 end
